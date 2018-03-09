93a18f703f1aafef4f436bc3d4edf78e7e00a89b
Dwijay Bane <dwijaybane2@gmail.com>
2018-03-10 04:22:36 +0530

    YTdownload updated with logfile support

--- YTDownloadUtility/download_yt_vids.py
+++ YTDownloadUtility/download_yt_vids.py
@@ -21,13 +21,14 @@ if sys.argv > 1:
     vidFile = open(sys.argv[1], 'r') 
     vidList = vidFile.readlines()
     vidFile.close()
+    # Some sort of download failure check
+    # if fails retry 5 times
+    # if continued to fails put in list and display at end
+    failedVid = 0
+    logFile = open("download_yt.log", 'a')
     for count, vid in enumerate(vidList):
-        # Some sort of download failure check
-        # if fails retry 5 times
-        # if continued to fails put in list and display at end
         error_msg = 0
         timeout = False
-        failedVid = 0
         error_msg = os.system('youtube-dl -o "{0}-%(title)s.%(ext)s" -f 18 {1}'.format(count,vid))   # Download 360 pix video
         if error_msg != 0:
             for retry in range(4):
@@ -41,8 +42,12 @@ if sys.argv > 1:
             if timeout == True:
                 failedVid += 1
                 print '\n\nFailed after {0} retries: {1}'.format(retry+2,vid)
+                logFile.writelines('Failed after {0} retries: {1}'.format(retry+2,vid))
 
-print "\n\nTotal Failed videos: {0}".format(failedVid)
-print("------------- %s seconds -------------" % (time.time() - start_time))
-
-    
+    print "\n\nTotal Failed videos: {0}".format(failedVid)
+    print "------------- {0} seconds -------------".format(time.time() - start_time)
+    logFile.writelines("\n\nTotal Failed videos: {0}".format(failedVid))
+    logFile.writelines("\n------------- {0} seconds -------------".format(time.time() - start_time))
+    logFile.close()
+# else:
+#     print "Usage: {0} <links_file>".format(sys.argv[0])
